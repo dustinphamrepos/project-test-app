bec1df120b4eab23b33cd0bd4cf6d5815c170b04
UriPham <ducphamtrung.devw@gmail.com>
2023-03-13 09:42:58 +0700

    #51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Hoi dan IT</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Hoi dan IT</Navbar.Brand> */}
+                <NavLink to="/" className='navbar-brand'>Hoi dan IT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -18,13 +19,13 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
+                            <NavDropdown.Item>
                                 Log in
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item>
                                 Log out
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item>
                                 Profile
                             </NavDropdown.Item>
                         </NavDropdown>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            home page components
+        </div>
+    )
+}
+
+export default HomePage
