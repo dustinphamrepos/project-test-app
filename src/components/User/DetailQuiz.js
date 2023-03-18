bc2e5b9dee6686068f35b465d42c3fb1d81b07ae
UriPham <ducphamtrung.devw@gmail.com>
2023-03-18 14:44:38 +0700

    #84

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getDataQuiz } from "../../services/apiService";
+import { useParams, useLocation } from "react-router-dom";
 import _ from 'lodash'
+import { getDataQuiz } from "../../services/apiService";
+import './DetailQuiz.scss'
 
 const DetailQuiz = () => {
     const params = useParams()
+    const location = useLocation()
+    // console.log(location)
+
     // console.log('>>>>>>', params)
     const quizId = params.id
 
@@ -16,7 +20,7 @@ const DetailQuiz = () => {
         // console.log("check", res);
         if (res && res.EC === 0) {
             let raw = res.DT
-            console.log('...', raw)
+            // console.log('...', raw)
             let data = _.chain(raw)
                 // Group the elements of Array based on `color` property
                 .groupBy("id")
@@ -36,12 +40,38 @@ const DetailQuiz = () => {
                     return { questionId: key, answers, questionDescription, image }
                 })
                 .value()
-            console.log(data)
+            // console.log(data)
         }
     }
+
     return (
         <div className="detail-quiz-container">
-            Detail quiz
+            <div className="left-content">
+                <div className="title">
+                    Quiz {quizId}: {location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className="q-body">
+                    <img />
+                </div>
+                <div className="q-content">
+                    <div className="question">
+                            Question 1: How are u doing?
+                    </div>
+                    <div className="answer">
+                        <div className="a-child">A. iwuvjwkjvcjksw</div>
+                        <div className="b-child">A. iwuvjwkjvcjksw</div>
+                        <div className="c-child">A. iwuvjwkjvcjksw</div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button className="btn btn-secondary">Prev</button>
+                    <button className="btn btn-primary">Next</button>
+                </div>
+            </div>
+            <div className="right-content">
+                Count down
+            </div>
         </div>
     );
 }
